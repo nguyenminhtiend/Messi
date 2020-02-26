69b8eae5f48854e9f36dc2114a707772e80b9551
Tien Nguyen <nguyenminhtiend@gmail.com>
2020-02-26 17:47:31 +0700

    #

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+
 const helmet = require('helmet');
 const initRoutes = require('./routes');
 const { logRequest, logResponse, errorHandler } = require('./middlewares');
